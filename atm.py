459b76fe46e4c1d8a31fd73f578619073b53afa4
Walter Otsyula <wotsyula@gmail.com>
2021-10-24 15:06:49 +0300

    added login

--- atm.py
+++ atm.py
@@ -6,3 +6,32 @@ users = [
     {'username': 'collins', 'pin': '0003', 'balance': { 'KSh':-500, 'USD':2000 }},
     {'username': 'juma', 'pin': '0004', 'balance': { 'KSh':140, 'USD':200 }},
 ]
+
+def promptString(prompt):
+    print(prompt + ':')
+    result = input()
+    return result
+
+def findUser(username, pin):
+    for user in users:
+        if (user.get('username') == username and user.get('pin') == pin):
+            return user
+    
+    return None
+
+def login():
+    while (True):
+        username = promptString('Enter user name')
+
+        if (username):
+            pin = promptString('Enter your PIN number')
+
+            print('Logging in...')
+            user = findUser(username, pin)
+
+            if (user):
+                return user
+
+user = login()
+
+print(user)
