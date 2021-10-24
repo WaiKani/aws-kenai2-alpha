5d7144fac3cc8d2f6e365aabbb8bb741d7079696
Walter Otsyula <wotsyula@gmail.com>
2021-10-24 14:46:58 +0300

    added user dict array

---
+++ atm.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+users = [
+    {'username': 'walter', 'pin': '0001', 'balance': { 'KSh':2000, 'USD':0 }},
+    {'username': 'elaine', 'pin': '0002', 'balance': { 'KSh':1000, 'USD':200 }},
+    {'username': 'collins', 'pin': '0003', 'balance': { 'KSh':-500, 'USD':2000 }},
+    {'username': 'juma', 'pin': '0004', 'balance': { 'KSh':140, 'USD':200 }},
+]
