660cc13eccab1a72975ac7e4d4996a1ed94ded9f
Walter Otsyula <wotsyula@gmail.com>
2021-10-24 16:18:59 +0300

    added menu and withdraw

--- atm.py
+++ atm.py
@@ -7,10 +7,17 @@ users = [
     {'username': 'juma', 'pin': '0004', 'balance': { 'KSh':140, 'USD':200 }},
 ]
 
+ksh_notes = [50, 100, 200, 500, 1000]
+usd_notes = [1, 5, 20, 50, 100]
+transactions = []
+
 def promptString(prompt):
     print(prompt + ':')
     result = input()
     return result
+
+def promptNumber(prompt):
+    return int(promptString(prompt))
 
 def findUser(username, pin):
     for user in users:
@@ -32,6 +39,83 @@ def login():
             if (user):
                 return user
 
+def promptAmmount(min, balance):
+    print('Your balance is:', balance)
+
+    # Make sure there is a balance we can withdraw
+    if (balance < min):
+        return 0
+
+    ammount = promptNumber('How much would you like to withdraw (x' + str(min) + ')')
+
+    # Make sure ammount is greater than or equal to min and
+    # Make sure ammount is divisible by min exactly
+    if (ammount < min or ammount % min > 0):
+        print('Please enter a number that is a multiple of:', min)
+        return 0
+
+    # Make sure ammount is in balance
+    if (ammount > balance):
+        print('Not enough funds. Balance:', balance)
+        return 0
+
+    return ammount
+
+def withdrawKSH(user):
+    min = ksh_notes[3]
+    balance = user.get('balance', {}).get('KSh')
+    ammount = promptAmmount(min, balance)
+
+    # Check for error
+    if (ammount > 0):
+        user.get('balance', {}).update({ 'KSh': balance - ammount })
+        transactions.append({ 'ACTION': 'WITHDRAW', 'AMMOUNT': ammount, 'CURRENCY': 'KSH' })
+
+def withdrawUSD(user):
+    min = usd_notes[2]
+    balance = user.get('balance').get('USD')
+    ammount = promptAmmount(min, balance)
+
+    # Check for error
+    if (ammount > 0):
+        user.get('balance').update({ 'USD': balance - ammount })
+        transactions.append({ 'ACTION': 'WITHDRAW', 'AMMOUNT': ammount, 'CURRENCY': 'USD' })
+
+def withdraw(user):
+    currency = promptString('What would you like to withdraw (Ksh/USD)').upper()
+
+    if (currency == 'KSH'):
+        withdrawKSH(user)
+    elif (currency == 'USD'):
+        withdrawUSD(user)
+    else:
+        print('Invalid currency')
+
+def balance(user):
+    pass
+
+def menu(user):
+    print('--------------------------')
+    print('MENU')
+    print('--------------------------')
+    print('A: WITHDRAW')
+    print('B: CHECK BALANCE')
+    print('Q: QUIT')
+
+    command = promptString('What would you like to do (A/B/Q)').upper()
+
+    if command == 'A':
+        withdraw(user)
+        return True
+    elif command == 'B':
+        balance(user)
+        return True
+    elif command == 'Q':
+        return False
+
+        return True
+
 user = login()
 
-print(user)
+while (menu(user)):
+    pass
