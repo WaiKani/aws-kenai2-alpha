57d53f60f34f2a055e9143bf9b832716de9d7a63
PeterMule <19330314+PeterMule@users.noreply.github.com>
2021-10-29 15:01:46 +0300

    Peter mule patch 1 (#1)
    * Update atm.py
    
    Compendium of Changes
    +Added comments
    +Inserted code to clear screen after each login attempt
    +Inserted code to limit number of login attempts to 3, if fail exit program
    =Moved atm initialization into 'if __main__' clause
    
    * Added y/n choice for withdraw and deposit
    
    * add newline at end of file
    
    Co-authored-by: OgadaOgada <collinceogada@gmail.com>
    Co-authored-by: Walter Otsyula <wotsyula@gmail.com>

--- atm.py
+++ atm.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-
 from datetime import date
+import os
 
+#Dict serving as the database of the atm
 users = [
     {'username': 'walter', 'pin': '0001', 'balance': { 'KSh':2000, 'USD':0 }},
     {'username': 'elaine', 'pin': '0002', 'balance': { 'KSh':1000, 'USD':200 }},
@@ -9,10 +10,21 @@ users = [
     {'username': 'juma', 'pin': '0004', 'balance': { 'KSh':140, 'USD':200 }},
 ]
 
+
+#A list denoting the dominations for each currency
 ksh_notes = [50, 100, 200, 500, 1000]
 usd_notes = [1, 5, 20, 50, 100]
+
+#A history of transaction histpry
 transactions = []
 
+########
+#Stream Handler Functions
+########
+
+#This group of functions serve as the standard iostream handlers
+#Takes in the prompt to be displayed on stdout
+#Returns the user input in the format expected
 def promptString(prompt):
     print(prompt + ':')
     result = input()
@@ -21,6 +33,21 @@ def promptString(prompt):
 def promptNumber(prompt):
     return int(promptString(prompt))
 
+
+
+########
+#Logging In Functions
+########
+
+#Screen Clearing Function
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+#Searches the database for a username and pin match
+#If the user if not found or the pin is incorrect returns None
 def findUser(username, pin):
     for user in users:
         if (user.get('username') == username and user.get('pin') == pin):
@@ -28,19 +55,36 @@ def findUser(username, pin):
     
     return None
 
+#Login Subproces
+#Logs in a user by verifying the user and corresponding pin
+#A maximum of 3 attempts is allowed
 def login():
-    while (True):
+    counter = 0
+    while (counter < 3):
         username = promptString('Enter user name')
 
         if (username):
             pin = promptString('Enter your PIN number')
+
+            counter = counter + 1
+            clearConsole()
 
             print('Logging in...')
             user = findUser(username, pin)
 
             if (user):
                 return user
+    promptString("You have made to may attempts. Try again later")
+    exit()
 
+
+
+
+########
+#Withdrawal Functions
+########
+
+#Prompts the user on the amount to withdraw while ensuring no over draft
 def promptAmmount(min, balance):
     print('Your balance is:', balance)
 
@@ -63,6 +107,7 @@ def promptAmmount(min, balance):
 
     return ammount
 
+#Withdraw Ksh from account
 def withdrawKSH(user):
     min = ksh_notes[3]
     balance = user.get('balance', {}).get('KSh')
@@ -70,12 +115,13 @@ def withdrawKSH(user):
 
     # Check for error
     if ammount > 0:
-        command = promptString('Withdraw ' + str(ammount) + ' KSH').upper()
+        command = promptString('Withdraw ' + str(ammount) + ' KSH (y/n)').upper()  #added the y/n options
 
         if command == 'Y':
             user.get('balance', {}).update({ 'KSh': balance - ammount })
             transactions.append({ 'ACTION': 'WITHDRAW', 'AMMOUNT': ammount, 'CURRENCY': 'KSH' })
 
+#Withdraw USD from account
 def withdrawUSD(user):
     min = usd_notes[2]
     balance = user.get('balance').get('USD')
@@ -89,6 +135,8 @@ def withdrawUSD(user):
             user.get('balance').update({ 'USD': balance - ammount })
             transactions.append({ 'ACTION': 'WITHDRAW', 'AMMOUNT': ammount, 'CURRENCY': 'USD' })
 
+#Widthrawl suprocess
+#Allows user to either USD or KSH
 def withdraw(user):
     print('--------------------------')
     print('WITHDRAW')
@@ -102,6 +150,14 @@ def withdraw(user):
     else:
         print('Invalid currency')
 
+
+
+########
+#Deposit Functions
+########
+
+#Cash Deposit Subprocess
+#Allows the user to deposit Ksh into their account in multiples of 500
 def deposit(user):
     print('--------------------------')
     print('DEPOSIT')
@@ -122,12 +178,20 @@ def deposit(user):
         print('Please enter a number that is a multiple of:', min)
         return
 
-    command = promptString('Deposit ' + str(ammount) + ' KSH').upper()
+    command = promptString('Deposit ' + str(ammount) + ' KSH  (y/n)').upper() #added the y/n options
 
     if command == 'Y':
         user.get('balance', {}).update({ 'KSh': balance + ammount })
         transactions.append({ 'ACTION': 'DEPOSIT', 'AMMOUNT': ammount, 'CURRENCY': 'KSH' })
 
+
+
+########
+#Balance Function
+########
+
+#Acount Balance Display Subprocess
+#Allows user to view the amount in thier account of each currency
 def balance(user):
     print('--------------------------')
     print('BALANCE')
@@ -137,6 +201,15 @@ def balance(user):
     print('--------------------------')
     promptString('Press enter to continue')
 
+
+
+########
+#Recipt Display Function
+########
+
+#Reciept Printing Function
+#Allows the user to print out a copy of all transactions carried out
+#while interacting with the ATM
 def receipt(user):
     while (True):
         printReceipt = promptString('Do you want a receipt (y/n)').upper()
@@ -162,6 +235,15 @@ def receipt(user):
     print('--------------------------')
     print(date.today())
 
+
+
+########
+#Central Menu Function
+########
+
+#Menu Displaying function
+#Once a user logs in this function displays and the possible actions
+#the user can do and calls the appropriate subprocess
 def menu(user):
     print('--------------------------')
     print('MENU')
@@ -184,9 +266,15 @@ def menu(user):
 
     return True
 
-user = login()
 
-while (menu(user)):
-    pass
+if __name__ == '__main__':
+    #Attempt to login the user
+    user = login()
 
-receipt(user)
+    #display the menu
+    while (menu(user)):
+        pass
+
+    #on quiting prompt the user if they want a reciept
+    receipt(user)
+    
